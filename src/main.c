3c9de324a4f19788369bd4f897ab059ccdf27569
Raphael Weinhart <raphaelweinhart@ws-95-165.haw-1x.haw-hamburg.de>
2022-12-13 16:49:57 +0100

    update

--- src/main.c
+++ src/main.c
@@ -1,10 +1,33 @@
-int main(int argc, char const *argv[])
+
+#define TASK 2
+
+// prototypes
+void task_0(int argc, char *argv[]);
+void task_1(int argc, char *argv[]);
+void task_2(int argc, char *argv[]);
+
+int main(int argc, char *argv[])
 {
-    /* code */
-    for (int i = 0; i < 12; i++)
+    int task = TASK;
+
+    if (task == 0)
     {
-        /* code */
+        // call funktion for task 0
+    }
+    else if (task == 1)
+    {
+        // call funktion for task 1
+    }
+    else if (task == 2)
+    {
+        // call funktion for task 2
     }
     
+    
     return 0;
+}
+
+void task_1(int argc, char *argv[])
+{
+    
 }
